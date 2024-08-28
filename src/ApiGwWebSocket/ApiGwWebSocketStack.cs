38598274958e553d20ddb7a245b77d3c354b2f58
Neil Mackey <neilmackey@gmail.com>
2024-08-28 20:17:19 +0100

    Initial commit

---
+++ src/ApiGwWebSocket/ApiGwWebSocketStack.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+using Constructs;
+
+namespace ApiGwWebSocket
+{
+    public class ApiGwWebSocketStack : Stack
+    {
+        internal ApiGwWebSocketStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
