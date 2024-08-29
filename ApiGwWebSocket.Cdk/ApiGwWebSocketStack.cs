c520cfd2916b4afe40ca04902eb473af579c085b
Neil Mackey <neilmackey@gmail.com>
2024-08-29 21:17:01 +0100

    Wire up default route. Send a placeholder response back

--- ApiGwWebSocket.Cdk/ApiGwWebSocketStack.cs
+++ ApiGwWebSocket.Cdk/ApiGwWebSocketStack.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using Amazon.CDK;
 using Amazon.CDK.AWS.Apigatewayv2;
 using Amazon.CDK.AWS.Lambda;
+using Amazon.CDK.AWS.Logs;
 using Amazon.CDK.AwsApigatewayv2Integrations;
 using Constructs;
 
@@ -12,7 +13,7 @@ namespace ApiGwWebSocket
         internal ApiGwWebSocketStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
         {
             // The code that defines your stack goes here
-            var lambdaFunction = new Function(this, "MyFirstLambda", new FunctionProps
+            var lambdaFunction = new Function(this, "ClientConnectionHandler", new FunctionProps
             {
                 Runtime = Runtime.DOTNET_8,
                 Handler = "Connect.Lambda::Connect.Lambda.Function::Handler",
@@ -24,23 +25,45 @@ namespace ApiGwWebSocket
                     ["POWERTOOLS_SERVICE_NAME"] = "websocket-connect-handler",
                     ["POWERTOOLS_LOG_LEVEL"] = "Debug",
                     ["POWERTOOLS_LOGGER_LOG_EVENT"] = "true"
-                }
-            });
-            
-            var webSocketApi = new WebSocketApi(this, "webSocketAPi", new WebSocketApiProps
+                },
+                LogGroup = new LogGroup(this, "LogGroup", new LogGroupProps
                 {
+                    RemovalPolicy = RemovalPolicy.DESTROY,
+                    Retention = RetentionDays.ONE_WEEK
+                })
+            });
+
+            var webSocketApi = new WebSocketApi(this, "webSocketAPi", new WebSocketApiProps
+            {
                 Description = "My WebSocket API",
                 ConnectRouteOptions = new WebSocketRouteOptions
                 {
                     Integration = new WebSocketLambdaIntegration("connect", lambdaFunction)
                 },
+                DisconnectRouteOptions = new WebSocketRouteOptions
+                {
+                    Integration = new WebSocketLambdaIntegration("disconnect", lambdaFunction)
+                },
+                DefaultRouteOptions = new WebSocketRouteOptions
+                {
+                    Integration = new WebSocketLambdaIntegration("default", lambdaFunction)
+                },
             });
             
+            webSocketApi.GrantManageConnections(lambdaFunction);
+
             var stage = new WebSocketStage(this, "stage", new WebSocketStageProps
             {
                 StageName = "dev",
                 WebSocketApi = webSocketApi,
                 AutoDeploy = true
+            });
+            
+            lambdaFunction.AddEnvironment("WEBSOCKET_URL", $"{webSocketApi.ApiEndpoint}/{stage.StageName}");
+            
+            _ = new CfnOutput(this, "WebSocketURL", new CfnOutputProps
+            {
+                Value = $"{webSocketApi.ApiEndpoint}/{stage.StageName}"
             });
         }
     }
--- Connect.Lambda/Function.cs
+++ Connect.Lambda/Function.cs
@@ -1,4 +1,6 @@
-﻿using Amazon.Lambda.APIGatewayEvents;
+﻿using Amazon.ApiGatewayManagementApi;
+using Amazon.ApiGatewayManagementApi.Model;
+using Amazon.Lambda.APIGatewayEvents;
 using Amazon.Lambda.Core;
 using AWS.Lambda.Powertools.Logging;
 
@@ -8,10 +10,25 @@ namespace Connect.Lambda;
 
 public class Function
 {
+    private static AmazonApiGatewayManagementApiClient _apiGatewayManagementApiClient = new(
+        new AmazonApiGatewayManagementApiConfig()
+        {
+            ServiceURL = Environment.GetEnvironmentVariable("WEBSOCKET_URL")
+        });
+    
     [Logging]
     public async Task<APIGatewayProxyResponse> Handler(APIGatewayProxyRequest request, ILambdaContext context)
     {
-        context.Logger.LogLine(request.RequestContext.ConnectionId);
+        if (request.RequestContext.EventType == "MESSAGE")
+        {
+            Logger.LogInformation(new { request.RequestContext.ConnectionId, request.RequestContext.EventType }, "Received message from client: {0}", request.Body);
+            
+            _ = await _apiGatewayManagementApiClient.PostToConnectionAsync(new PostToConnectionRequest
+            {
+                ConnectionId = request.RequestContext.ConnectionId,
+                Data = new MemoryStream("Hello from the server!"u8.ToArray())
+            });
+        }
         
         return await Task.FromResult(new APIGatewayProxyResponse { StatusCode = 200 });
     }
